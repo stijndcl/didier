eb182b71f47397bfc9b40bd89d1fcd2c3ee45bd7
stijndcl <declercq.stijn@outlook.com>
2022-06-17 01:02:36 +0200

    Create connection fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
+import os
+from typing import AsyncGenerator
+
 import pytest
 
 from alembic import command, config
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.engine import engine
 
 
 @pytest.fixture(scope="session")
@@ -9,7 +15,27 @@ def tables():
     Starts from an empty database and runs through all the migrations to check those as well
     while we're at it
     """
+    print("CWD: ", os.getcwd())
     alembic_config = config.Config("alembic.ini")
     command.upgrade(alembic_config, "head")
     yield
     command.downgrade(alembic_config, "base")
+
+
+@pytest.fixture
+async def database_session(tables) -> AsyncGenerator[AsyncSession, None]:
+    """Fixture to create a session for every test
+    Rollbacks the transaction afterwards so that the future tests start with a clean database
+    """
+    connection = await engine.connect()
+    transaction = await connection.begin()
+    session = AsyncSession(bind=connection, expire_on_commit=False)
+
+    yield session
+
+    # Clean up session & rollback transactions
+    await session.close()
+    if transaction.is_valid:
+        await transaction.rollback()
+
+    await connection.close()
