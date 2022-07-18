0c810d84e9906489c5baf8a7f79f4145ef0147d4
stijndcl <declercq.stijn@outlook.com>
2022-07-18 23:22:28 +0200

    Google search

--- didier/cogs/other.py
+++ didier/cogs/other.py
@@ -1,7 +1,10 @@
+from discord import app_commands
 from discord.ext import commands
 
 from didier import Didier
 from didier.data.apis import urban_dictionary
+from didier.data.embeds.google import GoogleSearch
+from didier.data.scrapers import google
 
 
 class Other(commands.Cog):
@@ -15,8 +18,18 @@ class Other(commands.Cog):
     @commands.hybrid_command(name="define", description="Urban Dictionary", aliases=["Ud", "Urban"], usage="[Woord]")
     async def define(self, ctx: commands.Context, *, query: str):
         """Look up the definition of a word on the Urban Dictionary"""
-        definitions = await urban_dictionary.lookup(self.client.http_session, query)
-        await ctx.reply(embed=definitions[0].to_embed(), mention_author=False)
+        async with ctx.typing():
+            definitions = await urban_dictionary.lookup(self.client.http_session, query)
+            await ctx.reply(embed=definitions[0].to_embed(), mention_author=False)
+
+    @commands.hybrid_command(name="google", description="Google search", usage="[Query]")
+    @app_commands.describe(query="Search query")
+    async def google(self, ctx: commands.Context, *, query: str):
+        """Google something"""
+        async with ctx.typing():
+            results = await google.google_search(self.client.http_session, query)
+            embed = GoogleSearch(results).to_embed()
+            await ctx.reply(embed=embed, mention_author=False)
 
 
 async def setup(client: Didier):
---
+++ didier/data/embeds/google/google_search.py
@@ -0,0 +1,48 @@
+from http import HTTPStatus
+
+import discord
+
+from didier.data.embeds.base import EmbedBaseModel
+from didier.data.scrapers.google import SearchData
+
+__all__ = ["GoogleSearch"]
+
+
+class GoogleSearch(EmbedBaseModel):
+    """Embed to display Google search results"""
+
+    data: SearchData
+
+    def __init__(self, data: SearchData):
+        self.data = data
+
+    def _error_embed(self) -> discord.Embed:
+        """Custom embed for unsuccessful requests"""
+        embed = discord.Embed(colour=discord.Colour.red())
+        embed.set_author(name="Google Search")
+
+        # Empty embed
+        if not self.data.results:
+            embed.description = "Geen resultaten gevonden"
+            return embed
+
+        # Error embed
+        embed.description = f"Status {self.data.status_code}"
+
+        return embed
+
+    def to_embed(self) -> discord.Embed:
+        if not self.data.results or self.data.status_code != HTTPStatus.OK:
+            return self._error_embed()
+
+        embed = discord.Embed(colour=discord.Colour.blue())
+        embed.set_author(name="Google Search")
+        embed.set_footer(text=self.data.result_stats or None)
+
+        # Add all results into the description
+        results = []
+        for index, url in enumerate(self.data.results):
+            results.append(f"{index + 1}: {url}")
+
+        embed.description = "\n".join(results)
+        return embed
