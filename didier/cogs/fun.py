8fb990cea82ed45e40f4b249076631372c2d74b6
stijndcl <declercq.stijn@outlook.com>
2022-08-26 18:32:53 +0200

    Add missing translations, memegen message command

--- didier/cogs/fun.py
+++ didier/cogs/fun.py
@@ -1,3 +1,5 @@
+import shlex
+
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -5,6 +7,7 @@ from discord.ext import commands
 from database.crud.dad_jokes import get_random_dad_joke
 from database.crud.memes import get_meme_by_name
 from didier import Didier
+from didier.data.apis.imgflip import generate_meme
 from didier.views.modals import GenerateMeme
 
 
@@ -31,16 +34,29 @@ class Fun(commands.Cog):
             return await ctx.reply(joke.joke, mention_author=False)
 
     @commands.group(name="Memegen", aliases=["Meme", "Memes"], invoke_without_command=True, case_insensitive=True)
-    async def memegen_ctx(self, ctx: commands.Context):
+    async def memegen_ctx(self, ctx: commands.Context, meme_name: str, *, fields: str):
         """Command group for meme-related commands"""
+        async with ctx.typing():
+            async with self.client.postgres_session as session:
+                result = await get_meme_by_name(session, meme_name)
+
+            if result is None:
+                return await ctx.reply(f"Found no meme matching `{meme_name}`.", mention_author=False)
+
+            meme = await generate_meme(self.client.http_session, result, shlex.split(fields))
+            if meme is None:
+                return await ctx.reply("Something went wrong.", mention_author=False)
+
+            return await ctx.reply(meme)
 
     @memes_slash.command(name="generate", description="Generate a meme")
     async def memegen_slash(self, interaction: discord.Interaction, meme: str):
         """Slash command to generate a meme"""
         async with self.client.postgres_session as session:
             result = await get_meme_by_name(session, meme)
-            if result is None:
-                return await interaction.response.send_message(f"Found no meme matching `{meme}`.", ephemeral=True)
+
+        if result is None:
+            return await interaction.response.send_message(f"Found no meme matching `{meme}`.", ephemeral=True)
 
         modal = GenerateMeme(self.client, result)
         await interaction.response.send_modal(modal)
