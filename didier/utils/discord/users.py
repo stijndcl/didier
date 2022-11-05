9e9be39358b419d6f3066e1a0f4ab11eeb284a9f
stijndcl <declercq.stijn@outlook.com>
2022-11-05 22:26:48 +0100

    Only show upcoming deadlines for courses the user is actually subscribed to

--- didier/utils/discord/users.py
+++ didier/utils/discord/users.py
@@ -2,10 +2,26 @@ from typing import Union
 
 import discord
 
+from database.schemas import UforaCourse
 from didier import Didier
 from didier.exceptions import NotInMainGuildException
 
-__all__ = ["to_main_guild_member"]
+__all__ = ["has_course", "to_main_guild_member"]
+
+
+def has_course(member: discord.Member, course: UforaCourse) -> bool:
+    """Check if a member is taking a Ufora course"""
+    for role in member.roles:
+        if role.id == course.role_id:
+            return True
+
+        if course.overarching_role_id is not None and course.overarching_role_id == role.id:
+            return True
+
+        if course.alternative_overarching_role_id is not None and course.alternative_overarching_role_id == role.id:
+            return True
+
+    return False
 
 
 def to_main_guild_member(client: Didier, user: Union[discord.User, discord.Member]) -> discord.Member:
