617e69b7a350201e767e5d85f3c7c4281adfa8d8
stijndcl <declercq.stijn@outlook.com>
2023-01-16 21:33:58 +0100

    Keep the original message when sniping edits

--- didier/didier.py
+++ didier/didier.py
@@ -382,6 +382,12 @@ class Didier(commands.Bot):
         if not should_snipe(before):
             return
 
+        # If the edited message is currently present in the snipe cache,
+        # don't update the <before>, but instead change the <after>
+        existing = self.sniped.get(before.channel.id, None)
+        if existing is not None and existing[0].id == before.id:
+            before = existing[0]
+
         self.sniped[before.channel.id] = (
             before,
             after,
